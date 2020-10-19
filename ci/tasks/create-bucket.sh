1c8729ee95ff534754a2d89aa4a700c2863f0ef4
Colin Simmons <colin.simmons@engineerbetter.com>
2020-10-19 23:00:59 +0100

    add region constraint to create bucket call

--- ci/tasks/create-bucket.sh
+++ ci/tasks/create-bucket.sh
@@ -5,7 +5,9 @@ set -euo pipefail
 aws s3api head-bucket \
   --bucket "${BUCKET}" \
   || aws s3api create-bucket \
-  --bucket "${BUCKET}"
+  --bucket "${BUCKET}" \
+  --region "${AWS_DEFAULT_REGION}" \
+  --create-bucket-configuration LocationConstraint="${AWS_DEFAULT_REGION}"
 
 aws s3api put-bucket-versioning \
   --bucket "${BUCKET}" \
