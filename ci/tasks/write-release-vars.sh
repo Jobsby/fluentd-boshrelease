f5a0482b8a9c4be87671e114ebf3e2b3d4d30d66
Colin Simmons <colin.simmons@engineerbetter.com>
2020-10-20 16:40:32 +0100

    add shasum to release notes

--- ci/tasks/write-release-vars.sh
+++ ci/tasks/write-release-vars.sh
@@ -4,6 +4,10 @@ set -euo pipefail
 
 version=$(cat version/version)
 name="fluentd-boshrelease v${version}"
+release_name=$(bosh int fluentd-boshrelease/config/final.yml --path /final_name)
+release_url="https://github.com/EngineerBetter/fluentd-boshrelease/releases/download/${version}/fluentd-final-release-${version}.tgz"
+release_tgz="final-release/fluentd-final-release-${version}.tgz"
+release_sha1=$(sha1sum "${release_tgz}" | head -n1 | awk '{print $1}')
 
 echo "${name}" > release-vars/name
 
@@ -15,4 +19,13 @@ echo "$commit" > release-vars/commit
 
 cat << EOF > release-vars/body
 Auto-generated release
+
+### Deployment
+\`\`\`yaml
+releases:
+- name:    $release_name
+  version: $version
+  url:     $release_url
+  sha1:    $release_sha1
+\`\`\`
 EOF
