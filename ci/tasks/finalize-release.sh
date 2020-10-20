7c5de1a942935448e83e60d09981f500c08d3c55
Colin Simmons <colin.simmons@engineerbetter.com>
2020-10-20 12:37:27 +0100

    create github releases

--- ci/tasks/finalize-release.sh
+++ ci/tasks/finalize-release.sh
@@ -8,22 +8,14 @@ cp version/number bumped-version/number
 export ROOT_PATH=$PWD
 PROMOTED_REPO=$PWD/final-fluentd-boshrelease
 
-export FINAL_RELEASE_PATH=$ROOT_PATH/final-release/*.tgz
+export FINAL_RELEASE_PATH="${ROOT_PATH}/final-release/*.tgz"
 
 git config --global user.email "ci@localhost"
 git config --global user.name "CI Bot"
 
-pushd ./fluentd-boshrelease
-  tag_name="v${VERSION}"
+git clone ./fluentd-boshrelease "${PROMOTED_REPO}"
 
-  tag_annotation="Final release ${VERSION} tagged via concourse"
-
-  git tag -a "${tag_name}" -m "${tag_annotation}"
-popd
-
-git clone ./fluentd-boshrelease $PROMOTED_REPO
-
-pushd $PROMOTED_REPO
+pushd "${PROMOTED_REPO}"
   git status
 
   git checkout master
@@ -37,7 +29,7 @@ blobstore:
     credentials_source: env_or_profile
 EOF
 
-  bosh finalize-release --version $VERSION $FINAL_RELEASE_PATH
+  bosh finalize-release --version "${VERSION}" "${FINAL_RELEASE_PATH}"
 
   git add -A
   git status
---
+++ ci/tasks/write-release-vars.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+version=$(cat version/version)
+name="fluentd-boshrelease v${version}"
+
+echo "${name}" > release-vars/name
+
+pushd control-tower
+  commit=$(git rev-parse HEAD)
+popd
+
+echo "$commit" > release-vars/commit
+
+cat << EOF > release-vars/body
+Auto-generated release
+EOF
