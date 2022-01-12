24793d90aaa00e303e614bb8861e4e0256891252
Andy Paine <andy.paine@engineerbetter.com>
2022-01-12 14:32:24 +0000

    Add optional command line args
    Defaults to --no-supervisor to keep backwards compatibility

--- spec/fluentd_spec.rb
+++ spec/fluentd_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'yaml'
 require 'bosh/template/test'
 
 describe 'fluentd job' do
@@ -6,6 +7,21 @@ describe 'fluentd job' do
   let(:release_dir) { File.join(File.dirname(__FILE__), '..') }
   let(:release) { Bosh::Template::Test::ReleaseDir.new(release_dir) }
   let(:job) { release.job('fluentd') }
+
+  describe 'bpm.yml' do
+    let(:template) {job.template('config/bpm.yml')}
+
+    it 'defaults to using --no-supervisor' do
+        process = YAML::load(template.render({}))['processes'][0]
+        expect(process['args']).to match(['--no-supervisor'])
+    end
+
+    it 'users user-defined args when supplied' do
+        args = ['--foo', 'bar']
+        process = YAML::load(template.render({'command_line_args' => args}))['processes'][0]
+        expect(process['args']).to match(['--foo', 'bar'])
+    end
+  end
 
   describe 'ca.crt' do
     let(:template) {job.template('certs/ca.crt')}
