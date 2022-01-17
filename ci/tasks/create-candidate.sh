b0678f8904de514a1541f818010c9d2bd17d2eed
Andy Paine <andy.paine@engineerbetter.com>
2022-01-17 09:57:18 +0000

    Colin knows better than me

--- ci/tasks/create-candidate.sh
+++ ci/tasks/create-candidate.sh
@@ -5,6 +5,6 @@ set -e
 export ROOT_PATH=$PWD
 
 cd fluentd-boshrelease
-rm config/private.yml # Breaks dev releases
+rm config/private.yml
 
 bosh create-release --tarball="${ROOT_PATH}/release/fluentd-dev-release.tgz" --timestamp-version --force
--- ci/tasks/package-fluentd.sh
+++ ci/tasks/package-fluentd.sh
@@ -4,6 +4,13 @@ fluentd_path=$(cat fluentd.version)
 tarball="${fluentd_path##*/}.tgz"
 tar czvpf "$tarball" vendor fluentd.Gemfile fluentd.Gemfile.lock
 bosh add-blob $tarball fluentd.tgz
+cat >> config/private.yml <<EOF
+---
+blobstore:
+  provider: s3
+  options:
+    credentials_source: env_or_profile
+EOF
 bosh upload-blobs
 
 git config --global user.email "ci@localhost"
