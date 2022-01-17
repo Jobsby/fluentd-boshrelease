a0c4632bb0693100d551a3fdcda822780e02e7ad
Andy Paine <andy.paine@engineerbetter.com>
2022-01-17 09:52:53 +0000

    Remove private.yml in candidate builds

--- ci/tasks/create-candidate.sh
+++ ci/tasks/create-candidate.sh
@@ -5,5 +5,6 @@ set -e
 export ROOT_PATH=$PWD
 
 cd fluentd-boshrelease
+rm config/private.yml # Breaks dev releases
 
 bosh create-release --tarball="${ROOT_PATH}/release/fluentd-dev-release.tgz" --timestamp-version --force
