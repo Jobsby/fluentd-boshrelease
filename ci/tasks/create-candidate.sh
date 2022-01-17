20f2d06d0317e7abe2881b723324cf0ad1a8f3fb
Andy Paine <andy.paine@engineerbetter.com>
2022-01-17 10:00:38 +0000

    Remove line not just comment

--- ci/tasks/create-candidate.sh
+++ ci/tasks/create-candidate.sh
@@ -5,6 +5,5 @@ set -e
 export ROOT_PATH=$PWD
 
 cd fluentd-boshrelease
-rm config/private.yml
 
 bosh create-release --tarball="${ROOT_PATH}/release/fluentd-dev-release.tgz" --timestamp-version --force
