68abbb5dc5655277c3e73652a96ffc6688bea8ac
Cai Cooper <cagiti@users.noreply.github.com>
2022-06-30 11:13:35 +0100

    Omit ruby test packages

--- ci/tasks/vendor-ruby.sh
+++ ci/tasks/vendor-ruby.sh
@@ -14,7 +14,8 @@ pushd ruby-release
   latest_ruby_version="$(bosh blobs | grep ruby-3 | cut -d . -f 1-2 | sort | tail -1)"
   # I wanted to do `grep name packages/ruby-${latest_ruby_version}-r*/spec`
   # but globbing didn't seem to work in the container
-  latest_ruby_version_full="$(find packages -type d | awk -F/ -v ruby_version="${latest_ruby_version}" '($0 ~ ruby_version){print $2}')"
+  # Let's ignore test versions
+  latest_ruby_version_full="$(find packages -type d -not -name "*test" | awk -F/ -v ruby_version="${latest_ruby_version}" '($0 ~ ruby_version){print $2}')"
 popd
 
 pushd "${VENDORED_REPO}"
